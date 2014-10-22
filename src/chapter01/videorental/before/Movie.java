6d11a2e53dd6f0e94d03845aff2ef880fe3c7e60
Joke(김미리내) <joke@coupang.com>
2014-10-23 00:24:30 +0900

    파일 수정

--- src/chapter01/videorental/before/Movie.java
+++ src/chapter01/videorental/before/Movie.java
@@ -3,7 +3,7 @@ package chapter01.videorental.before;
 public class Movie {
     public static final int CHILDRENS = 2;
     public static final int REGULAR = 0;
-    public static final int NEW_REGULAR = 1;
+    public static final int NEW_RELEASE = 1;
     private String _title;
     private int _priceCode;
 
